0caf2f626f0ca212d3fd0a0b1ec8067a62651dca
Maximilian Rohleder <Maxi.Rohleder@fau.de>
2022-10-19 21:53:33 +0200

    Calculate Fundamental in Dataloader
    - was previously computed on the fly
    - saved precious main-thread time by beeing done by dataloader worker

--- data/loader.py
+++ data/loader.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import numpy as np
+from fume import calculate_fundamental_matrix
 from torch.utils.data import Dataset
 import torch
 
@@ -17,8 +18,12 @@ class NPZData(Dataset):
         sample = np.load(str(self.files[item]))
 
         x_tensor = torch.from_numpy(sample['x'])
-        P_tensor = torch.from_numpy(sample['P'])
         y_tensor = torch.from_numpy(sample['y'])
+
+        # compute fundamental matrices from projection matrices
+        F12 = calculate_fundamental_matrix(P_src=sample['P'][0], P_dst=sample['P'][1])
+        F21 = calculate_fundamental_matrix(P_src=sample['P'][1], P_dst=sample['P'][0])
+        P_tensor = torch.tensor(np.array([F12, F21]), dtype=torch.float32)
 
         if self.x_transform is not None:
             x_tensor = self.x_transform(x_tensor)
