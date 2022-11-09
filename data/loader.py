be151f0d1a8082519cd34e4dd8282156739bb240
Maximilian Rohleder <Maxi.Rohleder@fau.de>
2022-11-09 17:42:03 +0100

    Refactored Dataloader
    * Fundamental matrix adaption takes place in contructor
    * no need to calculate this each time

--- data/loader.py
+++ data/loader.py
@@ -4,12 +4,6 @@ from fume import calculate_fundamental_matrix
 from torch.utils.data import Dataset
 import torch
 
-
-FLIPLR = np.asarray([[-1, 0, 975],
-                     [0, 1, 0],
-                     [0, 0, 1]])
-
-
 def downsample_tensor(ten: torch.Tensor, d, p):
     return torch.nn.functional.pad(ten[:, ::d, ::d], (p, p, p, p), mode="constant")
 
@@ -19,13 +13,20 @@ class NPZData(Dataset):
         self.files = sorted(list(Path(path).glob('*.npz')))
         self.downsample, self.pad = downsample, pad
 
+        # calculate how to adapt the projection matrices
+        centered = np.eye(3)
+        centered[:2, 2] = - (976 / 2 - 0.5)  # the Siemens matrices map onto detector of size 976
+        scale = np.diag([1 / self.downsample, 1 / self.downsample, 1])  # scales the detector size
+        FLIPLR = np.asarray([[-1, 0, 975],
+                             [0, 1, 0],
+                             [0, 0, 1]])  # specifically for Siemens matrices
+        self.adapt = scale @ centered @ FLIPLR
+
     def __len__(self):
         return len(self.files)
 
     def __getitem__(self, item):
         sample = np.load(str(self.files[item]))
-        x_shape = sample['x'].shape
-
         # images need to be transposed for fume layers
         x_tensor = torch.from_numpy(np.transpose(sample['x'], axes=(0, 2, 1)))
         y_tensor = torch.from_numpy(np.transpose(sample['y'], axes=(0, 2, 1)))
@@ -36,11 +37,8 @@ class NPZData(Dataset):
             y_tensor = downsample_tensor(y_tensor, self.downsample, self.pad)
 
         # adapt projection matrices to shape
-        centered = np.eye(3)
-        centered[:2, 2] = - ((np.asarray(x_shape[1:]) / 2) - 0.5)
-        scale = np.diag([1 / self.downsample, 1 / self.downsample, 1])
-        P1 = scale @ centered @ FLIPLR @ sample['P'][0]
-        P2 = scale @ centered @ FLIPLR @ sample['P'][1]
+        P1 = self.adapt @ sample['P'][0]
+        P2 = self.adapt @ sample['P'][1]
 
         # compute fundamental matrices from projection matrices
         F12 = calculate_fundamental_matrix(P_src=P1, P_dst=P2)
